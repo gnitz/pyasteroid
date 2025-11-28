11ba7e36fde8f0436bcf2102fe807a91dc22880c
gnitz <gnit1989@gmail.com>
2025-11-28 21:09:44 +0000

    implemented sprites and player movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame;
 from logger import log_state;
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH;
+from player import Player;
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}");
@@ -9,8 +10,23 @@ def main():
 
     pygame.init();
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT));
+    
+    gclock = pygame.time.Clock();
+    dt = 0;
 
-    while(True):
+    updatable = pygame.sprite.Group();
+    drawable = pygame.sprite.Group();
+
+    Player.containers = (updatable, drawable);
+
+    p = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+
+    
+
+
+    # MAIN GAME LOOP
+
+    while(True): 
         log_state();
 
         for event in pygame.event.get():
@@ -18,8 +34,13 @@ def main():
                 return;
 
         screen.fill("black");
+        for sprite in drawable:
+            sprite.draw(screen);
+        
         pygame.display.flip();
-
+        dt = gclock.tick(60)/1000;
+        updatable.update(dt);
+        #print(dt);
 
 if __name__ == "__main__":
     main()
